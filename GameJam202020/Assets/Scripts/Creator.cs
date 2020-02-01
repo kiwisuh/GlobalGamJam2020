aae7d13b4c5960f36838294eea9f165d0ca1bb66
kiwisuh <hsuh78@gmail.com>
2020-02-01 10:14:55 -0500

    Kiwi Push

--- GameJam202020/Assets/Scripts/Creator.cs
+++ GameJam202020/Assets/Scripts/Creator.cs
@@ -1,18 +1,67 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class Creator : MonoBehaviour
 {
+
+	private int creatorType;
+	public Image inventory;
+	public Image mheal;
+	public Image mbecaon;
+	public Image mturret;
+	public GameObject player;
+	public Material newMat;
+	public bool showInventory;
     // Start is called before the first frame update
     void Start()
     {
-        
+		newMat = Resources.Load("Materials/HealthMaterial", typeof(Material)) as Material;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+			if(Input.GetKeyDown(KeyCode.I)){
+			showInventory = true;
+				selectOption();
+			}
+		if(showInventory){
+			if(Input.GetKeyDown(KeyCode.W)){
+				print("HHHHH");
+				createHealthPool();
+			}if(Input.GetKeyDown(KeyCode.A)){
+				createBeacon();
+			}if(Input.GetKeyDown(KeyCode.S)){
+				createTurret();
+			}
+		}
+
     }
+
+		public void selectOption(){
+		print("test");
+			inventory.color = new Color(1,1,1,1);
+			mheal.color = new Color(0,1,0,1);
+			mbecaon.color = new Color(0,0,1,1);
+			mturret.color = new Color(1,0,0,1);
+
+
+
+		}
+
+		public void createBeacon(){
+
+		}
+
+		public void createTurret(){
+
+		}
+
+		public void createHealthPool(){
+			print("HealthPOOL BITCH");
+			creatorType = 1;
+			player.GetComponent<MeshRenderer>().material = newMat;
+		}
 }
---
+++ GameJam202020/Assets/Scripts/Cube.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Cube : MonoBehaviour
+{
+  public float startHealth = 100;
+  private float health;
+  public int worth = 50;
+  public Image healthBar;
+    // Start is called before the first frame update
+    void Start()
+    {
+      health = startHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Damage(float amount){
+      healthBar.fillAmount = health / startHealth;
+      health -= amount;
+      if(health <= 0){
+        DestroyObject();
+      }
+    }
+
+    public void DestroyObject(){
+      Destroy(gameObject);
+    }
+}
