12753bb80c27b2c1921f26a48fabec4400ab078b
Ben <ben@brinkmanfamily.ca>
2020-02-01 20:35:06 -0500

    Ben's commit

--- GameJam2020/Assets/Scripts/PlacedBox.cs
+++ GameJam2020/Assets/Scripts/PlacedBox.cs
@@ -1,14 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlacedBox : MonoBehaviour
 {
 
     enum Mode {Turret, PulseTurret, Bomb, Beacon, Health, Buff};
     Mode mode;
-    public float level = 1;
+    public int level = 1;
+    
+    private Renderer rend;
 
+    public GameObject[] turretObj;
+    public GameObject[] healthObj;
+    public GameObject[] beaconObj;
+    public GameObject[] pulseTurretObj;
+    public GameObject[] bombObj;
+    public GameObject[] buffObj;
+    public GameObject stillObj1;
+    public GameObject stillObj2;
+
+    private CapsuleCollider capCol;
+
+    public GameObject currentObj;
+    public GameObject objHolder;
+
+    public GameObject TriImg;
 
     public float sightDistance;
     private Vector3 closestEnemyPos;
@@ -17,10 +35,19 @@ public class PlacedBox : MonoBehaviour
 
     public bool bensActivateButton;
 
+    //health
+    public float startHealth = 100;
+    private float health;
+    public Image healthBar;
+    public float healthIncrease;
+
     //Turret
     public GameObject projectile;
     public Transform gun;
+    public float gunDamage;
+    public float gunDamageIncrease;
     public float turretCooldown;
+    public float turretCooldownDecrease;
     private float timeLastShot;
 
     //pulseTurret
@@ -31,22 +58,46 @@ public class PlacedBox : MonoBehaviour
     public float explosionRadius;
     public float explosionDamage;
 
+    //buff
+    public float buffLength;
+    public float buffDamage;
+    public float buffDamageIncrease;
+    public float buffLengthIncrease;
+
+    //heal
+    public float healAmount;
+    public float healAmountIncrease;
+    private float lastTimeHealed;
+    public float healCooldown;
+    public float healCooldownDecrease;
+
     // Start is called before the first frame update
     void Start()
     {
-        mode = Mode.PulseTurret;
+        capCol = GetComponent<CapsuleCollider>();
+        health = startHealth;
         timeLastShot = Time.time - turretCooldown;
+        lastTimeHealed = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        rend = GetComponent<Renderer>();
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Damage(20);
+        }
         if (mode == Mode.Turret)
         {
+            
             DetectClosest("Enemy");
             if (active)
             {
                 this.transform.LookAt(closestEnemyPos);
+                stillObj1.transform.eulerAngles = Vector3.zero;
+                stillObj2.transform.eulerAngles = Vector3.zero;
                 if (Time.time - timeLastShot > turretCooldown)
                 {
                     timeLastShot = Time.time;
@@ -58,20 +109,67 @@ public class PlacedBox : MonoBehaviour
         {
             transform.eulerAngles += new Vector3(0.0f, rotateSpeed * Time.deltaTime, 0.0f);
 
+            stillObj1.transform.eulerAngles = Vector3.zero;
+            stillObj2.transform.eulerAngles = Vector3.zero;
             if (Time.time - timeLastShot > pulseTurretCooldown)
             {
                 timeLastShot = Time.time;
                 Shoot();
             }
         }
+        else if (mode == Mode.Buff)
+        {
+            GetNearbyPlayers();
+        }
+        else if (mode == Mode.Health)
+        {
+            if(Time.time - lastTimeHealed > healCooldownDecrease)
+            {
+                GetNearbyPlayers();
+            }
+        }
         
     }
 
+    public void Heal(float amount)
+    {
+        health += amount;
+        if (health > startHealth)
+        {
+            health = startHealth;
+        }
+        healthBar.fillAmount = health / startHealth;
+
+    }
+
+    private void GetNearbyPlayers()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, sightDistance);
+        if (hitColliders.Length > 0)
+        {
+            for (int i = 0; i < hitColliders.Length; i++)
+            {
+                if (hitColliders[i].gameObject.tag == "Player")
+                {
+                    if (mode == Mode.Buff)
+                    {
+                        hitColliders[i].SendMessage("BuffLength", buffLength);
+                        hitColliders[i].SendMessage("BuffDamage", buffDamage);
+                    }
+                    else if(mode == Mode.Health)
+                    {
+                        hitColliders[i].SendMessage("Heal", healAmount);
+                        lastTimeHealed = Time.time;
+                    }
+                }
+            }
+        }
+    }
 
     private void DetectClosest(string name)
     {
         Collider[] hitColliders = Physics.OverlapSphere(transform.position, sightDistance);
-        Debug.Log(hitColliders);
+
         if (hitColliders.Length > 0)
         {
             float dist = 0;
@@ -108,12 +206,65 @@ public class PlacedBox : MonoBehaviour
         }
     }
 
+    public void SetType(string type)
+    {
+        capCol = GetComponent<CapsuleCollider>();
+        if (type == "turret")
+        {
+            mode = Mode.Turret;
+            ChooseSkin();
+            stillObj1.SetActive(true);
+            stillObj2.SetActive(false);
+            capCol.radius = 2f;
+            //rend.material = turretMat;
+        }
+        else if (type == "pulseTurret")
+        {
+            mode = Mode.PulseTurret;
+            ChooseSkin();
+            stillObj2.SetActive(true);
+            stillObj1.SetActive(false);
+            capCol.radius = 2f;
+        }
+        else if (type == "beacon")
+        {
+            mode = Mode.Beacon;
+            ChooseSkin();
+            transform.tag = "Beacon";
+            //rend.material = beaconMat;
+            capCol.radius = 0.7f;
+        }
+        else if (type == "bomb")
+        {
+            transform.tag = "Beacon";
+            mode = Mode.Bomb;
+            ChooseSkin();
+            capCol.radius = 0.5f;
+
+        }
+        else if (type == "buff")
+        {
+            mode = Mode.Buff;
+            ChooseSkin();
+
+        }
+        else if (type == "health")
+        {
+            //rend.material = healthMat;
+            mode = Mode.Health;
+            ChooseSkin();
+            GetNearbyPlayers();
+
+        }
+    }
+
     public void Shoot()
     {
         if (mode == Mode.Turret)
         {
             GameObject laserFired = Instantiate(projectile, gun.position, Quaternion.AngleAxis(transform.eulerAngles.y, Vector3.up));
             laserFired.SendMessage("SetType", "bullet");
+            laserFired.SendMessage("DamageInput", gunDamage);
         }
         else if (mode == Mode.PulseTurret)
         {
@@ -133,22 +284,32 @@ public class PlacedBox : MonoBehaviour
 
                 GameObject laserFired = Instantiate(projectile, transform.position, Quaternion.AngleAxis(transform.eulerAngles.y + angle, Vector3.up));
                 laserFired.SendMessage("SetType", "bullet");
+                laserFired.SendMessage("DamageInput", gunDamage);
 
             }
-        }
-        else if (mode == Mode.Bomb)
-        {
-
         }
     }
 
     public void LevelUp()
     {
-        level += 1;
-        if(level > 3)
+        
+        if (level != 3)
         {
-            level = 3;
+            
+            level++;
+            //health
+            //heal amount, heal time, buff amount, buff time, damage, shoot speed, 
+            healAmount *= healAmountIncrease;
+            healCooldown -= healCooldownDecrease;
+            buffLength += buffLengthIncrease;
+            buffDamage *= buffDamageIncrease;
+            gunDamage *= gunDamageIncrease;
+            health += (healthIncrease *(level-1));
+            startHealth += (healthIncrease * (level - 1));
+            ChooseSkin();
+            
         }
+
     }
 
     public void Explode()
@@ -169,18 +330,95 @@ public class PlacedBox : MonoBehaviour
         
     }
 
-    public void Damage()
+    public void Damage(float dmg)
     {
+        health -= dmg;
+        healthBar.fillAmount = health / startHealth;
+
+        if (health < 0)
+        {
+            DestroyObject();
+        }
         if(mode == Mode.Bomb)
         {
             Explode();
         }
     }
 
-
+    public void Mod()
+    {
+        if(mode == Mode.Beacon)
+        {
+            mode = Mode.Bomb;
+            SetType("bomb");
+        }
+        else if (mode == Mode.Bomb)
+        {
+            mode = Mode.Beacon;
+            SetType("beacon");
+        }
+        else if (mode == Mode.Buff)
+        {
+            mode = Mode.Health;
+            SetType("health");
+        }
+        else if(mode == Mode.Health)
+        {
+            mode = Mode.Buff;
+            SetType("buff");
+        }
+        else if (mode == Mode.PulseTurret)
+        {
+            mode = Mode.Turret;
+            SetType("turret");
+        }
+        else if (mode == Mode.Turret)
+        {
+            mode = Mode.PulseTurret;
+            SetType("pulseTurret");
+        }
+    }
+    
     public void DestroyObject()
     {
         Destroy(gameObject);
     }
 
+    public void ChooseSkin()
+    {
+        foreach (Transform child in objHolder.transform)
+        {
+            child.gameObject.SetActive(false);
+        }
+
+        if (mode == Mode.Beacon)
+        {
+            beaconObj[level - 1].SetActive(true);
+        }
+        else if (mode == Mode.Bomb)
+        {
+            bombObj[level - 1].SetActive(true);
+        }
+        else if (mode == Mode.Buff)
+        {
+            buffObj[level - 1].SetActive(true);
+        }
+        else if (mode == Mode.Health)
+        {
+            healthObj[level - 1].SetActive(true);
+        }
+        else if (mode == Mode.PulseTurret)
+        {
+            pulseTurretObj[level - 1].SetActive(true);
+        }
+        else if (mode == Mode.Turret)
+        {
+            turretObj[level - 1].SetActive(true);
+        }
+    }
+
+    public void ToggleTriangle(bool on)
+    {
+        TriImg.SetActive(on);
+    }
 }
--- GameJam2020/Assets/Scripts/Player.cs
+++ GameJam2020/Assets/Scripts/Player.cs
@@ -1,16 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    public float startHealth = 100;
+    private float health;
+    public Image healthBar;
+
+    public GameObject renderObj;
+    private Vector3 startPos;
+
+    bool isDead = false;
+
+    public int scrap;
+    public int levelUpPrice;
+    public int modPrice;
+
     public int playerNumber = 1;
     public float speed;
-    private bool isDead = false;
+    private bool isDisabled = false;
     private Vector3 movementInput;
     private Vector3 turnInput;
     private Rigidbody rb;
+    private bool interactInput;
+
+    private string interactName;
     private string moveXAxisName;
     private string moveYAxisName;
     private string turnXAxisName;
@@ -25,10 +42,31 @@ public class Player : MonoBehaviour
     private bool gunDisable;
     private float lastTimeGunFired;
     public float gunCooldown;
+    public float damage;
+    public GameObject AttackObj;
+    public float rangeDamage;
+    public float rangeTick;
+
+    private float lastHealTick;
+    public float healCooldown;
+    public int healAmount;
 
     enum PlayerMode{ Creator, Healer, Engineer, Modder};
     PlayerMode mode;
 
+    //buff variables
+    private bool buffed;
+    public GameObject buffObj;
+    public float damageBoost;
+    private float boostCooldown;
+    private float lastBoostTime;
+
+    //Creator
+
+    public int objectCost;
+    public GameObject placeObject;
+    public GameObject creationUI;
+    private bool placementMode;
 
     // Start is called before the first frame update
     void Awake()
@@ -38,6 +76,14 @@ public class Player : MonoBehaviour
 
     private void Start()
     {
+        startPos = transform.position;
+        lastBoostTime = 0;
+        boostCooldown = 0;
+        buffed = false;
+        health = startHealth;
+        creationUI.SetActive(false);
+        buffObj.SetActive(false);
+        interactName = "Interact" + playerNumber;
         moveXAxisName = "LVertical" + playerNumber;
         moveYAxisName = "LHorizontal" + playerNumber;
         turnXAxisName = "RVertical" + playerNumber;
@@ -46,6 +92,8 @@ public class Player : MonoBehaviour
         cam = GameObject.FindGameObjectWithTag("CamRig");
         lastTimeGunFired = Time.time - gunCooldown;
         gunDisable = false;
+        damageBoost = 1;
+
         if (playerNumber == 1)
         {
             mode = PlayerMode.Creator;
@@ -68,48 +116,140 @@ public class Player : MonoBehaviour
     void Update()
     {
 
-        if (isDead)
-        {
-            rb.isKinematic = false;
-
-        }
-        else
-        {
-            rb.isKinematic = true;
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            mode = PlayerMode.Creator;
-            Debug.Log("1");
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            Debug.Log("2");
-
-            mode = PlayerMode.Healer;
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            Debug.Log("3");
-
-            mode = PlayerMode.Engineer;
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha4))
-        {
-            Debug.Log("4");
-
-            mode = PlayerMode.Modder;
-        }
-
+        //Get input names
         movementInput.z = -Input.GetAxis(moveXAxisName);
         movementInput.x = Input.GetAxis(moveYAxisName);
         turnInput.x = Input.GetAxis(turnXAxisName);
         turnInput.z = -Input.GetAxis(turnYAxisName);
         fireInput = Input.GetAxis(fireAxisName);
-        Turn();
-        Move();
 
-        if(mode == PlayerMode.Creator || mode == PlayerMode.Engineer)
+
+        if (Input.GetButtonDown(interactName))
+        {
+            interactInput = true;
+        }
+        else
+        {
+            interactInput = false;
+        }
+
+        if(mode == PlayerMode.Healer)
+        {
+            if (Input.GetButton(interactName))
+            {
+                interactInput = true;
+            }
+            else
+            {
+                interactInput = false;
+            }
+        }
+
+        print(interactInput);
+        //make sure they aint dead
+        if (isDisabled)
+        {
+            rb.isKinematic = true;
+            
+        }
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Respawn();
+        }
+        else
+        {
+            rb.isKinematic = false;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            mode = PlayerMode.Creator;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+
+            mode = PlayerMode.Healer;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+
+            mode = PlayerMode.Engineer;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+
+            mode = PlayerMode.Modder;
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Damage(20);
+        }
+
+
+
+        if(Time.time - lastBoostTime > boostCooldown)
+        {
+            buffObj.SetActive(false);
+            boostCooldown = 0;
+            damageBoost = 1.0f;
+        }
+
+        if (placementMode)
+        {
+            bool placed = false;
+            string message = "";
+            if (Input.GetButtonDown("X" + playerNumber))
+            {
+                placed = true;
+                message = "turret";
+            }
+            else if (Input.GetButtonDown("Y" + playerNumber))
+            {
+                placed = true;
+                message = "health";
+            }
+            else if (Input.GetButtonDown("B" + playerNumber))
+            {
+                placed = true;
+                message = "beacon";
+            }
+            if (placed)
+            {
+                GameObject newObject = Instantiate(placeObject, transform.position+transform.forward, Quaternion.identity);
+                newObject.SendMessage("SetType", message);
+                creationUI.SetActive(false);
+                placementMode = false;
+            }
+        }
+        else
+        {
+            Turn();
+            Move();
+        }
+        
+
+
+        if (mode == PlayerMode.Creator)
+        {
+            if (fireInput > 0 && Time.time - lastTimeGunFired > gunCooldown)
+            {
+                Shoot();
+                lastTimeGunFired = Time.time;
+            }
+            if (interactInput && scrap >= objectCost)
+            {
+                scrap -= objectCost;
+
+                creationUI.SetActive(true);
+                placementMode = true;
+            }
+        }
+        else if(mode == PlayerMode.Healer)
+        {
+
+        }
+        else if (mode == PlayerMode.Engineer)
         {
             if (fireInput > 0 && Time.time - lastTimeGunFired > gunCooldown)
             {
@@ -117,18 +257,56 @@ public class Player : MonoBehaviour
                 lastTimeGunFired = Time.time;
             }
         }
+
+
+        if (mode == PlayerMode.Modder || mode == PlayerMode.Healer)
+        {
+            print(fireInput);
+            if(fireInput > 0)
+            {
+                AttackObj.SetActive(true);
+                AttackObj.SendMessage("SetDamage", rangeDamage*damageBoost);
+                AttackObj.SendMessage("SetTick", rangeTick);
+            }
+            else
+            {
+                AttackObj.SetActive(false);
+            }
+        }
         else
         {
+            AttackObj.SetActive(false);
 
         }
-        
+    }
 
-        
+    public void BuffLength(float length)
+    {
+        lastBoostTime = Time.time;
+        boostCooldown = length;
+        buffObj.SetActive(true);
+    }
+
+    public void BuffDamage(float dmg)
+    {
+        damageBoost = dmg;
+    }
+
+    public void Heal(float amount)
+    {
+        health += amount;
+        if(health > startHealth)
+        {
+            health = startHealth;
+        }
+        healthBar.fillAmount = health / startHealth;
 
     }
 
-
-
+    public void AddScrap(int amount)
+    {
+        scrap += amount;
+    }
 
     private void Move()
     {
@@ -151,14 +329,97 @@ public class Player : MonoBehaviour
     {
         
         GameObject laserFired = Instantiate(projectile, gun.position, Quaternion.AngleAxis(playerAngle, Vector3.up));
+        
         if (mode == PlayerMode.Engineer)
         {
             laserFired.SendMessage("SetType", "sawblade");
-        }else if (mode == PlayerMode.Creator)
+            laserFired.SendMessage("DamageInput", (damage*2)* damageBoost);
+        }
+        else if (mode == PlayerMode.Creator)
         {
             laserFired.SendMessage("SetType", "nail");
-
+            laserFired.SendMessage("DamageInput", damage* damageBoost);
             //nail gun
+        }
+    }
+
+    public void Damage(float amount)
+    {
+        health -= amount;
+        healthBar.fillAmount = health / startHealth;
+        //Debug.Log(health);
+        if (health <= 0)
+        {
+            print("dead");
+            isDead = true;
+            isDisabled = true;
+            renderObj.SetActive(false);
+            //DestroyObject();
+        }
+    }
+
+    public void DestroyObject()
+    {
+        Destroy(gameObject);
+    }
+
+    public void Respawn()
+    {
+        transform.position = startPos;
+        health = startHealth;
+        isDead = false;
+        isDisabled = false;
+        renderObj.SetActive(true);
+        healthBar.fillAmount = health / startHealth;
+
+    }
+
+    private void OnTriggerStay(Collider col)
+    {
+        if (mode == PlayerMode.Engineer && (col.tag == "Object" || col.tag == "Beacon"))
+        {
+            col.gameObject.SendMessage("ToggleTriangle", true);
+
+            if (interactInput && scrap > levelUpPrice)
+            {
+                if (col.tag == "Object" || col.tag == "Beacon")
+                {
+                    scrap -= levelUpPrice;
+                    col.gameObject.SendMessage("LevelUp");
+                }
+            }
+        }
+        else if (mode == PlayerMode.Modder && (col.tag == "Object" || col.tag == "Beacon"))
+        {
+            col.gameObject.SendMessage("ToggleTriangle", true);
+
+            if (Input.GetButtonDown(interactName) && scrap > modPrice)
+            {
+                if (col.tag == "Object" || col.tag == "Beacon")
+                {
+                    scrap -= modPrice;
+                    col.gameObject.SendMessage("Mod");
+                }
+            }
+        }
+        else if (mode == PlayerMode.Healer && (col.tag == "Object" || col.tag == "Beacon"))
+        {
+            col.gameObject.SendMessage("ToggleTriangle", true);
+            if (interactInput && scrap > 0 && Time.time - lastHealTick > healCooldown)
+            {
+                lastHealTick = Time.time;
+                col.gameObject.SendMessage("Heal", healAmount);
+                scrap--;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider col)
+    {
+        if (col.tag == "Object" || col.tag == "Beacon")
+        {
+            col.gameObject.SendMessage("ToggleTriangle", false);
+            
         }
     }
 
